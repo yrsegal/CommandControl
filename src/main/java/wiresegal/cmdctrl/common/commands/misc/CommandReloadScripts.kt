10b8f1c4b1ab11eaac257704e59b7f57c7ad3ff5
yrsegal <yrsegal@gmail.com>
2016-12-05 21:41:53 -0500

    lock down reload command

--- src/main/java/wiresegal/cmdctrl/common/commands/misc/CommandReloadScripts.kt
+++ src/main/java/wiresegal/cmdctrl/common/commands/misc/CommandReloadScripts.kt
@@ -2,6 +2,8 @@ package wiresegal.cmdctrl.common.commands.misc
 
 import net.minecraft.command.CommandBase
 import net.minecraft.command.ICommandSender
+import net.minecraft.command.PlayerNotFoundException
+import net.minecraft.entity.player.EntityPlayerMP
 import net.minecraft.server.MinecraftServer
 import wiresegal.cmdctrl.common.config.ConfigLoader
 
@@ -11,7 +13,13 @@ import wiresegal.cmdctrl.common.config.ConfigLoader
  */
 object CommandReloadScripts : CommandBase() {
 
-    override fun execute(server: MinecraftServer, sender: ICommandSender, args: Array<out String>) = ConfigLoader.loadScripts(server)
+    override fun execute(server: MinecraftServer, sender: ICommandSender, args: Array<out String>) {
+        if (sender is EntityPlayerMP || sender is MinecraftServer) {
+            ConfigLoader.loadScripts(server)
+            notifyCommandListener(sender, this, "commandcontrol.reload.done")
+        } else
+            throw PlayerNotFoundException("commandcontrol.reload.hahanope")
+    }
     override fun getRequiredPermissionLevel() = 2
     override fun getCommandName() = "reloadcommandscripts"
     override fun getCommandUsage(sender: ICommandSender?) = "commandcontrol.reload.usage"
