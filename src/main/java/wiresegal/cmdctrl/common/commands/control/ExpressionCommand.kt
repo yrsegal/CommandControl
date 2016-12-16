ce5c162673ea02e3d9202369bd6114134b79f003
yrsegal <yrsegal@gmail.com>
2016-12-15 22:05:42 -0500

    fix build crash

--- src/main/java/wiresegal/cmdctrl/common/commands/control/ExpressionCommand.kt
+++ src/main/java/wiresegal/cmdctrl/common/commands/control/ExpressionCommand.kt
@@ -9,35 +9,27 @@ import java.math.BigDecimal
  */
 class ExpressionCommand(str: String) : Expression(str) {
     init {
-        addOperator(object : Operator("and", 2, false) {
-            override fun eval(v1: BigDecimal, v2: BigDecimal): BigDecimal {
-                return BigDecimal(v1.toBigInteger().and(v2.toBigInteger()))
-            }
-        })
-        addOperator(object : Operator("or", 2, false) {
-            override fun eval(v1: BigDecimal, v2: BigDecimal): BigDecimal {
-                return BigDecimal(v1.toBigInteger().or(v2.toBigInteger()))
-            }
-        })
-        addOperator(object : Operator("xor", 2, false) {
-            override fun eval(v1: BigDecimal, v2: BigDecimal): BigDecimal {
-                return BigDecimal(v1.toBigInteger().xor(v2.toBigInteger()))
-            }
-        })
-        addOperator(object : Operator("shl", 2, false) {
-            override fun eval(v1: BigDecimal, v2: BigDecimal): BigDecimal {
-                return BigDecimal(v1.toBigInteger().shiftLeft(v2.toInt()))
-            }
-        })
-        addOperator(object : Operator("shr", 2, false) {
-            override fun eval(v1: BigDecimal, v2: BigDecimal): BigDecimal {
-                return BigDecimal(v1.toBigInteger().shiftRight(v2.toInt()))
-            }
-        })
-        addFunction(object : Function("bitnot", 1) {
-            override fun eval(parameters: List<BigDecimal>): BigDecimal {
-                return BigDecimal(parameters[0].toBigInteger().not())
-            }
-        })
+        addOperator("and", 2, false) { v1, v2 -> BigDecimal(v1.toBigInteger().and(v2.toBigInteger())) }
+        addOperator("or", 2, false) { v1, v2 -> BigDecimal(v1.toBigInteger().or(v2.toBigInteger())) }
+        addOperator("xor", 2, false) { v1, v2 -> BigDecimal(v1.toBigInteger().xor(v2.toBigInteger())) }
+        addOperator("shl", 2, false) { v1, v2 -> BigDecimal(v1.toBigInteger().shiftLeft(v2.toInt())) }
+        addOperator("shr", 2, false) { v1, v2 -> BigDecimal(v1.toBigInteger().shiftRight(v2.toInt())) }
+        addFunction("bitnot", 1) { BigDecimal(it[0].toBigInteger().not()) }
+    }
+
+    fun addOperator(name: String, precedence: Int, leftAssoc: Boolean, impl: (v1: BigDecimal, v2: BigDecimal) -> BigDecimal) {
+        addOperator(OperatorCommand(name, precedence, leftAssoc, impl))
+    }
+
+    fun addFunction(name: String, paramCount: Int, impl: (List<BigDecimal>) -> BigDecimal) {
+        addFunction(FunctionCommand(name, paramCount, impl))
+    }
+
+    inner class OperatorCommand(name: String, precedence: Int, leftAssoc: Boolean, val impl: (v1: BigDecimal, v2: BigDecimal) -> BigDecimal) : Operator(name, precedence, leftAssoc) {
+        override fun eval(v1: BigDecimal, v2: BigDecimal) = impl(v1, v2)
+    }
+
+    inner class FunctionCommand(name: String, paramCount: Int, val impl: (List<BigDecimal>) -> BigDecimal) : Function(name, paramCount) {
+        override fun eval(parameters: List<BigDecimal>) = impl(parameters)
     }
 }
