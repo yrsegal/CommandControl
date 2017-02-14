9a48065df10fe9fc9ff6cd9018608d6433975865
yrsegal <yrsegal@gmail.com>
2017-02-14 15:21:34 -0500

    update liblib

--- src/main/java/wiresegal/cmdctrl/common/core/ScoreStorage.kt
+++ src/main/java/wiresegal/cmdctrl/common/core/ScoreStorage.kt
@@ -1,5 +1,6 @@
 package wiresegal.cmdctrl.common.core
 
+import net.minecraft.util.IntHashMap
 import java.util.*
 
 /**
@@ -8,16 +9,20 @@ import java.util.*
  *
  * A Type alias for pretty usage.
  */
-class ScoreStorage : HashMap<String, Int>()
+class ScoreStorage : MutableMap<String, Int> by HashMap() {
+    fun forEach(call: (String, Int) -> Unit) = forEach { call(it.key, it.value) }
+}
 
 /**
  * Another type alias, for easy getting.
  */
-interface IScoreMap<T: Any> : MutableMap<T, ScoreStorage>
+interface IScoreMap<T: Any> : MutableMap<T, ScoreStorage> {
+    fun forEach(call: (T, ScoreStorage) -> Unit) = forEach { call(it.key, it.value) }
+}
 
-class ScoreMap<T: Any> : HashMap<T, ScoreStorage>(), IScoreMap<T> {
+class ScoreMap<T: Any>(val map: MutableMap<T, ScoreStorage> = HashMap()) : MutableMap<T, ScoreStorage> by map, IScoreMap<T> {
     operator override fun get(key: T): ScoreStorage {
-        var gotten = super.get(key)
+        var gotten = map[key]
 
         if (gotten == null) {
             gotten = ScoreStorage()
@@ -28,9 +33,9 @@ class ScoreMap<T: Any> : HashMap<T, ScoreStorage>(), IScoreMap<T> {
     }
 }
 
-class WeakScoreMap<T: Any> : WeakHashMap<T, ScoreStorage>(), IScoreMap<T> {
+class WeakScoreMap<T: Any>(val map: MutableMap<T, ScoreStorage> = WeakHashMap()) : MutableMap<T, ScoreStorage> by map, IScoreMap<T> {
     operator override fun get(key: T): ScoreStorage {
-        var gotten = super.get(key)
+        var gotten = map.get(key)
 
         if (gotten == null) {
             gotten = ScoreStorage()
